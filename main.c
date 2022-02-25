56574ab71a0297c5f9a791926634eb63b54d6629
AnthonyLeconte <98951813+AnthonyLeconte@users.noreply.github.com>
2022-02-25 10:22:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 // Signal to be processing 
 #include "sig.h"
+
  
 // Constant 
 #define FFT_SIZE 512
@@ -16,6 +17,7 @@ float frequency_analyis(){
     // Half DFT is sufficient 
     int half_dft = SIZE/2;
     // Instantiate the 3 buffers 
+
     float* BASE_SINE [SIZE];
     float* BASE_COSINE [SIZE];
     float* out_fft [SIZE];
@@ -32,6 +34,7 @@ float frequency_analyis(){
         float i_dft = 0;
         for (int k = 0; k < SIZE; k++){
             // Accumulate 
+            ...
             r_dft = r_dft + BASE_COSINE[k]*sig[k];
             r_dft = i_dft + BASE_SINE[k]*sig[k];
         }
@@ -43,6 +46,7 @@ float frequency_analyis(){
     float maxVal = 0; 
     for (int i =0 ; i < half_dft; i++){
        // Use maxPos and maxVal  
+        ... 
        if (maxVal<out_fft[i]){
           maxVal = out_fft[i];
           maxPos = i;
@@ -54,13 +58,38 @@ float frequency_analyis(){
     printf("Associated frequency is  %f Hz\n",f);
     return f;
 }
+@@ -55,24 +60,24 @@ float amplitude_extraction(float f){
+    // Define step size 
+    float mu = 0.005;
+    // Define buffers 
+    float* u = malloc(SIZE*sizeof(float)); //ref cos2pi f n)*w
+    float* w = malloc(SIZE*sizeof(float)); // coef variable 
+    //float* e = malloc(SIZE*sizeof(float));  // coef erreur
 
-
-
+    // Instantiate w & u 
+    for (int n = 0; n < SIZE; n++){
+        w[n] = 0;
+        u[n] = cos(2*PI*f*n);
+    }
+    // LMS Algorithm 
+    for (int n = 0; n < SIZE; n++){
+       w[n+1]=w[n]-mu*w[n]*(SIG[n]-w[n]*u[n]);
+        ...
+    }
+    // Compute final estimate 
+    int mean_size = 500;
+    float amp = 0;
+    for (int n = 0; n < mean_size; n++){
+        // Mean on last samples 
+        amp+=w[SIZE-mean_size+n]/(mean_size);
+        ...
+    }
+    printf("Estimated amplitude is %f \n",amp);
+    return amp;
+}
 int main(){
     printf("--- Frequency analysis and amplitude extraction\n");
     float f = frequency_analyis();
     float a = amplitude_extraction(f);
     return 0;
-
 }
